48ba526bc9465e5e8ae4e892ea8cb8df0b254c9b
geodude3 <77406516+geodude3@users.noreply.github.com>
2023-01-18 00:14:53 -0800

    Can now report lights

--- src/pages/ReportLights.js
+++ src/pages/ReportLights.js
@@ -1,10 +1,40 @@
 import React from "react";
 import "../styles/App.css"
+import Axios from "axios";
 
 function ReportLights() {
+
+    const url = "https://iiksserver.herokuapp.com/lights";
+
+    const [room, setRoom] = React.useState(null,{
+        room:""
+    });
+
+    const handleChange = (e) => {
+        const newdata = {...room};
+        newdata[e.target.id] = e.target.value;
+        setRoom(newdata)
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        Axios.post(url,{
+            item:room.room
+        })
+        .then((res) => {
+            console.log(res.data)
+        })
+       
+    };
+    
+
     return(
         <div className="body">
             <h1>Welcome to the ReportLights website</h1>
+            <form onSubmit={handleSubmit}>
+                <input onChange={handleChange} type="text" placeholder="Room #" id="room"></input>
+                <input type="submit"></input>
+            </form>
         </div>
     )
 }
