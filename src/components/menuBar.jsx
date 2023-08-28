275c32f619c3c6146411dfe25951f97ad7e58790
geodude3 <77406516+geodude3@users.noreply.github.com>
2023-08-28 09:36:22 -0700

    .

--- src/components/menuBar.jsx
+++ src/components/menuBar.jsx
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import "./compStyles/menuBar.css";
 import logo from "../images/iiks.jpg"
 class MenuBar extends Component {
-    state = {  } 
-    render() { 
+    state = {}
+    render() {
 
         return (
             <React.Fragment>
                 <div id='menuBar'>
+                    <Link className='menuElement' to="/projects"><button className='btn btn-primary btn-lg'>Research</button></Link>
                     <Link className='menuElement' to="/api"><button className='btn btn-primary btn-lg'>Chat/API</button></Link>
                     <Link className='menuElement' to="/contact"><button className='btn btn-primary btn-lg'>Contact Us</button></Link>
                     <Link className='menuElement' to="/"><button className='btn btn-primary btn-lg'><img id='logoMenuBar' src={logo}></img></button></Link>
                     <Link className='menuElement' to="/about"><button className='btn btn-primary btn-lg'>About</button></Link>
                     <Link className='menuElement' to="/reportlights"><button className='btn btn-primary btn-lg'>Report Lights</button></Link>
-                    {/* <Link className='menuElement' to="/projects"><button className='btn btn-primary btn-lg'>Projects</button></Link>
-                    <Link className='menuElement' to="/sports"><button className='btn btn-primary btn-lg'>Sports</button></Link> */}
+                    <Link className='menuElement' to="/sports"><button className='btn btn-primary btn-lg'>Sports</button></Link>
 
                 </div>
             </React.Fragment>
         );
     }
 }
- 
+
 export default MenuBar;
