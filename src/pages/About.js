9c2b071c198ea40027e2d77cdc6655e8a366c6dc
geodude3 <77406516+geodude3@users.noreply.github.com>
2023-08-28 09:32:41 -0700

    .

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,23 +1,23 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import "../styles/about.css"
 
 import "../styles/App.css"
 
 function About() {
-    return(
+    return (
         <div className="body">
             <div id="links">
-                <Link className='menuElement' to="/projects"><button className='btn btn-danger btn-lg'>Projects</button></Link>
+                <Link className='menuElement' to="/projects"><button className='btn btn-danger btn-lg'>IIKS Research</button></Link>
                 <Link className='menuElement' to="/sports"><button className='btn btn-danger btn-lg'>Sports</button></Link>
             </div>
 
             <h1>About IIKS</h1>
             <p>
 
-                IIKS is an organisation created in 2016. 
-                Our operations, ranging from airline management to our contributions in the ion propulsion sector, are very diverse. 
-                IIKS has a number of high performance sports teams including ski racing, mountain biking, sailing, soccer 
+                IIKS is an organisation created in 2016.
+                Our operations, ranging from airline management to our contributions in the EHD propulsion sector, are very diverse.
+                IIKS has a number of high performance sports teams including ski racing, mountain biking, sailing, soccer
                 and more - see sports tab for more info.
             </p>
         </div>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -8,7 +8,7 @@ import EHD3 from "../images/EHD3.jpg"
 function Projects() {
     return (
         <div className="body">
-            <h1>Welcome to to Projects website</h1>
+            <h1>Welcome to IIKS Research</h1>
             <div>
                 IIKS is a leader in the field of Electrohydrodynamic (EHD), or ionic, propulsion. The lab has its own EHD thrusters as well as a 50kV 100W high voltage power converter (HVPC).
                 Latest research includes the optimisation of EHD thrusters with regard to electrode pair spacing.
