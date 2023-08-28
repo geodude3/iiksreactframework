1febc9d5170ecfde2dcb654dea42ac26f6b73932
geodude3 <77406516+geodude3@users.noreply.github.com>
2023-08-28 09:19:30 -0700

    ./

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,9 +3,13 @@ import "../styles/App.css"
 
 
 function Projects() {
-    return(
+    return (
         <div className="body">
             <h1>Welcome to to Projects website</h1>
+            <div>
+                IIKS is a leader in the field of Electrohydrodynamic (EHD), or ionic, propulsion. The lab has its own EHD thrusters as well as a 50kV 100W high voltage power converter (HVPC).
+                Latest research includes and
+            </div>
         </div>
     )
 }
