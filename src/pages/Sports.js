1fb2415b9e0496bf8ec5fd79cbb06662200ade0f
geodude3 <77406516+geodude3@users.noreply.github.com>
2023-09-17 10:55:13 -0700

    .

--- src/pages/Sports.js
+++ src/pages/Sports.js
@@ -2,9 +2,21 @@ import React from "react";
 import "../styles/App.css"
 
 function Sports() {
-    return(
+    return (
         <div className="body">
             <h1>Welcome to the Sports website</h1>
+            <div>
+                IIKS has a number of sports teams, which include:
+                <ul>
+                    <li>Ski racing</li>
+                    <li>Sailing</li>
+                    <li>Soccer</li>
+                    <li>Basketball</li>
+                    <li>Cross country</li>
+                    <li>Swimming</li>
+                    <li>Track and field</li>
+                </ul>
+            </div>
         </div>
     )
 }
